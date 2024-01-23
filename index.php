4b53031e20f0ccc047df25de6f6191cf496c5f78
Ednaldo <jead1966@gmail.com>
2024-01-23 20:11:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html
+	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel="stylesheet" type="text/css" href="./css/estilo.css" />
+	<title>AHMP</title>
+</head>
+
+<body>
+	<header class="cabecalho">
+		<div class="cabecalho__Logotipo">
+			<img src="./img/ahmp_circulo.png" height="100" width="110">
+		</div>
+		<div class="cabecalho__nome">
+			Associação de Handebol Máster da Paraíba - AHMP
+		</div>
+		<div class="cabecalho__link">
+			<a href="https://www.instagram.com/associacaohandmasterparaiba/" target="_blank"><img src="img/instagram_yellow.png" alt="Instagram"></a>
+			<a href="https://www.youtube.com/@associacaodehandebolmasterdapa" target="_blank"><img src="img/Youtube.png" alt="Youtube"></a>
+			<img src="img/whatsapp-orange.png" height="25px" width="25px">(83) 98843-2300
+		</div>
+	</header>
+	<nav class="menu">
+		<ul>
+			<li><a href="./index.php">Home</a></li>
+			<li><a href="#">Sobre a AHMP</a>
+				<ul>
+					<li><a href="#">Histórico</a></li>
+					<li><a href="docs/estatuto.pdf" target="_blank">Estatuto</a></li>
+					<li><a href="docs/organograma.pptx" target="_blank">Organograma</a></li>
+					<li><a href="#">Diretoria</a></li>
+					<li><a href="#">Conselho Fiscal</a></li>
+					<li><a href="#">Atas / Documentos</a></li>
+				</ul>
+			</li>
+			<li><a href="#" alt="Esportivo">Esportivo</a>
+				<ul>
+					<li><a href="#">Treinamentos</a></li>
+					<li><a href="#">Categorias</a></li>
+					<li><a href="#">Competições</a></li>
+					<li><a href="#">Calendário</a></li>
+				</ul>
+			</li>
+			<li><a href="#" alt="Midia">Mídia</a>
+				<ul>
+					<li><a href="#">Portifólio</a></li>
+					<li><a href="#">Logotipos</a></li>
+					<li><a href="#">Fotos / Vídeos</a></li>
+					<li><a href="#">Banners</a></li>
+				</ul>
+			</li>
+			<li><a href="#" alt="O Handebol">O Handebol</a>
+				<ul>
+					<li><a href="#">Regras</a></li>
+					<li><a href="#">A quadra</a></li>
+					<li><a href="#">Arbitragem</a></li>
+					<li><a href="#">O jogo</a></li>
+				</ul>
+			</li>
+			<li><a href="#" alt="Facilidades">Facilidades</a>
+				<ul>
+					<li><a href="#">Parceiros</a></li>
+					<li><a href="#">Dicas de Saúde</a></li>
+					<li><a href="#">Fisioterapeutas</a></li>
+					<li><a href="#">Clínicas</a></li>
+				</ul>
+			</li>
+			<li><a href="contact.html" alt="Contatos">Contatos</a></li>
+		</ul>
+	</nav>
+	<main class="corpo">
+		<div class="subtitulo">
+			Destaques
+		</div>
+		<div class="destaque">
+			<div class="destaque__imagem">
+				<a href="img/equipe55+.jpg" target="_blank"><img src="img/equipe55+.jpg" width="400px" height="300px" alt=""></a>
+				<figcaption>Equipe 55+</figcaption>
+			</div>
+			<div class="destaque__imagem">
+				<a href="img/equipe49+.jpg" target="_blank"><img src="img/equipe49+.jpg" width="400px" height="300px" alt=""></a>
+				<figcaption>Equipe 49+</figcaption>
+			</div>
+			<div class="destaque__imagem">
+				<a href="img/equipe35+.jpg" target="_blank"><img src="img/equipe35+.jpg" width="400px" height="300px" alt=""></a>
+				<figcaption>Equipe 35+</figcaption>
+			</div>
+		</div>
+		<div class="subtitulo">
+			Calendário
+		</div>
+		<div class="calendario">
+			<div class="calendario__item">
+				<img src="img/ligann.jpg" width="299px" height="312px" alt="">
+				<hr>
+			</div>
+			<div class="calendario__item">
+				<img src="img/copaamerica.jpg" width="299px" height="312px" alt="">
+				<hr>
+			</div>
+			<div class="calendario__item">
+				<img src="img/copajpa.png" width="299px" height="312px" alt="">
+				<hr>
+			</div>
+		</div>
+		<div class="subtitulo">
+			Galeria de Fotos e Vídeos
+		</div>
+		<div class="galeria">
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria_item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria_item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria_item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria_item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+		</div>
+		<div class="subtitulo">
+			Parceiros
+		</div>
+		<div class="parceiros">
+			<div class="parceiros__item">
+				<img src="img/ahmp.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/Logos/hbteam.png" alt="" width="200px" height="150px">
+			</div>
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+			<div class="galeria__item">
+				<img src="img/ahmp.png" alt="" width="45px" height="45px">
+			</div>
+		</div>
+		<div class="subtitulo">
+			Parceiros
+		</div>
+		<div class="parceiros">
+			<div class="parceiros__item">
+				<img src="img/ahmp.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/Logos/hbteam.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/Logos/dekra.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/Logos/etiquetasguararapes.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__itema">
+				<img src="img/Logos/foco.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/Logos/leopoldo.jpg" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/Logos/vistocar.png" alt="" width="200px" height="150px">
+			</div>
+			<div class="parceiros__item">
+				<img src="img/ahmp.png" alt="" width="200px" height="150px">
+			</div>
+		</div>
+	</main>
+	<footer class="rodape">
+		<div class="box__flags">
+			<img src="img/bandeiraBR.jpg" alt="">
+			<img src="img/bandeirapb.png" alt="">
+		</div>
+		<hr>
+		<div class="box__termos">
+			<ul>
+				<li>Acessibilidade |</li>
+				<li>Suporte |</li>
+				<li>Termos de Uso |</li>
+				<li>Política de Privacidade</li>
+			</ul>
+		</div>
+		<div class="box__copyright">
+			2024 Copyright - AHMP Handebol Master - Todos os Direitos Reservados
+		</div>
+	</footer>
+</body>
+
+</html>
